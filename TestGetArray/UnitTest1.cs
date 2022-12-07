1d1c739bde6674007d325e84c2d26d51a630e239
SadScream <sadscream.ilia@gmail.com>
2022-12-08 00:24:54 +0900

    add tests

--- TestGetArray/UnitTest1.cs
+++ TestGetArray/UnitTest1.cs
@@ -34,5 +34,152 @@ namespace TestGetArray
             var actual = Program.GetArray(5, 1, 5, 8, 21);
             CollectionAssert.AreEqual(expected, actual, new Comparer());
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestMethod2()
+        {
+            Program.GetArray(5, 1, 5, 1, 21);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestMethod3()
+        {
+            Program.GetArray(5, 1, 5, 12, 21);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestMethod4()
+        {
+            Program.GetArray(5, 1, 5, 5, 19);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestMethod5()
+        {
+            Program.GetArray(3, 3, 1, 8, 21);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestMethod6()
+        {
+            Program.GetArray(0, 1, 5, 8, 21);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestMethod7()
+        {
+            Program.GetArray(5, 1, 5, 5, 19.999);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            double[] expected = new double[]
+            {
+                -0,000799687,
+                -0,00103851,
+            };
+            var actual = Program.GetArray(2, 1, 5, 5, 20.001);
+            CollectionAssert.AreEqual(expected, actual, new Comparer());
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            double[] expected = new double[]
+            {
+                -0,055555556,
+            };
+            var actual = Program.GetArray(1, 1.570796327, 1.570796327, 3, 21);
+            CollectionAssert.AreEqual(expected, actual, new Comparer());
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            double[] expected = new double[]
+            {
+                -0,055555556,
+            };
+            var actual = Program.GetArray(1, -1.570796327, -1.570796327, 3, 21);
+            CollectionAssert.AreEqual(expected, actual, new Comparer());
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+            double[] expected = new double[]
+            {
+                -64.37616313,
+                -75.17245299,
+                -1.810606116,
+                -52.07291726,
+                -83.60176057
+            };
+            var actual = Program.GetArray(5, 1, 5, 1.001, 21);
+            CollectionAssert.AreEqual(expected, actual, new Comparer());
+        }
+
+        [TestMethod]
+        public void TestMethod12()
+        {
+            double[] expected = new double[]
+            {
+                64.37616313,
+                75.17245299,
+                1.810606116,
+                52.07291726,
+                83.60176057
+            };
+            var actual = Program.GetArray(5, 1, 5, 0.999, 21);
+            CollectionAssert.AreEqual(expected, actual, new Comparer());
+        }
+
+        [TestMethod]
+        public void TestMethod13()
+        {
+            double[] expected = new double[]
+            {
+                -64.37616313,
+                -75.17245299,
+                -1.810606116,
+                -52.07291726,
+                -83.60176057
+            };
+            var actual = Program.GetArray(5, 1, 5, 11.999, 21);
+            CollectionAssert.AreEqual(expected, actual, new Comparer());
+        }
+
+        [TestMethod]
+        public void TestMethod14()
+        {
+            double[] expected = new double[]
+            {
+                64.37616313,
+                75.17245299,
+                1.810606116,
+                52.07291726,
+                83.60176057
+            };
+            var actual = Program.GetArray(5, 1, 5, 12.001, 21);
+            CollectionAssert.AreEqual(expected, actual, new Comparer());
+        }
+
+        [TestMethod]
+        public void TestMethod15()
+        {
+            double[] expected = new double[]
+            {
+                0,
+            };
+            var actual = Program.GetArray(1, 0, 0, 3, 21);
+            CollectionAssert.AreEqual(expected, actual, new Comparer());
+        }
     }
 }
